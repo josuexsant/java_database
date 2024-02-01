ab4068a3c8763a272b18c0a857dced077dfa7461
Josue Santamaria <josuexsanta@gmail.com>
2024-02-01 12:26:21 -0600

    Se modifico perfomCreate

--- src/main/java/CrudOperations.java
+++ src/main/java/CrudOperations.java
@@ -1,45 +1,44 @@
-import java.sql.Connection;
-import java.sql.ResultSet;
-import java.sql.SQLException;
-import java.sql.Statement;
+import java.sql.*;
+
 public class CrudOperations {
     static CreateConnection createConn;
-    public static void main(String[] args){
+
+    public static void main(String[] args) {
         createConn = new CreateConnection();
-        performCreate();
+        performCreate("12.x","Test","Insercion de prueba");
         performRead();
-        performUpdate();
-        performDelete();
         System.out.println("-- Final State --");
         performRead();
     }
 
-    private static void performCreate(){
+    private static void performCreate(String recipeNumber, String title, String description) {
         String sql = "INSERT INTO RECIPES VALUES(" +
-                "NULL, " +
-                "'12-5', " +
-                "'Performing CRUD Operations', " +
-                "'How to perform create, read, update, delete functions')";
+                "null, ?,?,?)";
 
-        try (Connection conn = createConn.getConnection();
-             Statement stmt = conn.createStatement();) {
-            // Returns row-count or 0 if not successful
-            int result = stmt.executeUpdate(sql);
-            /**
-             * IMPORTANTE: Hace falta un parentesis dentro del if,
-             * ya se agrego.
-             */
-            if (result == 1) {
-                System.out.println("-- Record created --");
-            }else {
-                System.err.println("!! Record NOT Created !!");
+        /**
+         * Cambiamos los Statement objects por PreparedStament objects
+         * Statement stmt = conn.createStatement();) {
+         */
+        try (Connection conn = createConn.getConnection();) {
+            try (PreparedStatement pstmt = conn.prepareStatement(sql);) {
+                pstmt.setString(1, recipeNumber);
+                pstmt.setString(2, title);
+                pstmt.setString(3, description);
+                pstmt.executeUpdate();
+                int result = pstmt.executeUpdate(sql);
+                // Returns row-count or 0 if not successful
+                if (result == 1) {
+                    System.out.println("-- Record created --");
+                } else {
+                    System.err.println("!! Record NOT Created !!");
+                }
             }
-        } catch (SQLException e) {
-            e.printStackTrace();
+        } catch (SQLException ex) {
+            ex.printStackTrace();
         }
     }
 
-    private static void performRead(){
+    private static void performRead() {
         String qry = "select recipe_number, recipe_name, description from apressbooks.recipes";
         try (Connection conn = createConn.getConnection();
              Statement stmt = conn.createStatement();) {
@@ -55,14 +54,14 @@ public class CrudOperations {
         }
     }
 
-    private static void performUpdate(){
+    private static void performUpdate() {
         String sql = "UPDATE RECIPES " +
                 "SET RECIPE_NUMBER = '12-5' " +
                 "WHERE RECIPE_NUMBER = '12-4'";
         try (Connection conn = createConn.getConnection();
              Statement stmt = conn.createStatement();) {
             int result = stmt.executeUpdate(sql);
-            if (result == 1){
+            if (result == 1) {
                 System.out.println("-- Record Updated --");
             } else {
                 System.out.println("!! Record NOT Updated !!");
@@ -72,12 +71,12 @@ public class CrudOperations {
         }
     }
 
-    private static void performDelete(){
+    private static void performDelete() {
         String sql = "DELETE FROM RECIPES WHERE RECIPE_NUMBER = '12-5'";
         try (Connection conn = createConn.getConnection();
              Statement stmt = conn.createStatement();) {
             int result = stmt.executeUpdate(sql);
-            if (result > 0){
+            if (result > 0) {
                 System.out.println("-- Record Deleted --");
             } else {
                 System.out.println("!! Record NOT Deleted!!");
